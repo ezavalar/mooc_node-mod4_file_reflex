21e7491f05e4169700cfb7d3b4c1cf478cd0a841
Ericka Zavala <ezavalar_isc@hotmail.com>
2019-05-28 17:52:52 -0500

    Programa modificado

--- mod4_file_reflex.js
+++ mod4_file_reflex.js
@@ -1,7 +1,5 @@
-
     // Imports user module mod4-quote_of_the_day.js
-let my_mod = require("./mod4-quote_of_the_day");
-
+let my_mod = require("./mod4_quote_of_the_day.js");
 let fs = require("fs");   // Imports file system module
 
 console.log();
@@ -14,5 +12,13 @@ setTimeout(reflexes, delay);
 function reflexes() {
 
     // ... include code here
+    //Implementado por Ericka Zavala
+    let t= new Date();
+    console.log("Press return:");
+    process.stdin.on("data", function(line){
+    	console.log("Your time is: "+t.getMilliseconds()+"ms");
+    	process.exit(0);
+    });
+
 };
 
--- mod4_quote_of_the_day.js
+++ mod4_quote_of_the_day.js
@@ -1,2 +1,12 @@
+//Implementado por Ericka Zavala
+exports.quote_of_the_day= function()
+{
+	let fs = require("fs");
+	let content= fs.readFileSync("tests/mod4_quotes_of_the_day.json");
+	let jsonContent=JSON.parse(content);
+	console.log("quote_of_the_day:");
 
-
+	//Generamos numero random entre 0 y 3
+	let num= Math.floor(Math.random()*3);
+	console.log(jsonContent[num]);
+};
